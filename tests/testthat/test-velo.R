905807525d69d1e2cf88802c4550243b4e7e4562
HamitAhmat <hamitahmat75@gmail.com>
2026-04-13 19:33:03 +0200

    Question 20 : documentation et tests du paramètre filtre

--- tests/testthat/test-velo.R
+++ tests/testthat/test-velo.R
@@ -43,9 +43,9 @@ test_that("calcul_distribution_semaine retourne 7 jours", {
 })
 
 test_that("plot_distribution_semaine retourne un ggplot", {
-  resultat <- plot_distribution_semaine(df_velo)
+   resultat <- plot_distribution_semaine(df_velo)
 
-  expect_s3_class(resultat, "ggplot")
+   expect_s3_class(resultat, "ggplot")
 })
 
 test_that("filtrer_trajet filtre les bonnes boucles", {
@@ -68,4 +68,34 @@ test_that("filtrer_trajet retourne tout si boucle est NULL", {
   resultat <- filtrer_trajet(df_velo, boucle = NULL)
 
   expect_equal(nrow(resultat), nrow(df_velo))
+})
+
+
+test_that("calcul_distribution_semaine avec filtre TRUE", {
+  resultat <- calcul_distribution_semaine(df_velo, filtre = TRUE)
+
+  expect_s3_class(resultat, "data.frame")
+  expect_true("trajets" %in% colnames(resultat))
+  expect_lte(nrow(resultat), 7)
+})
+
+test_that("calcul_distribution_semaine avec filtre FALSE", {
+  resultat_filtre <- calcul_distribution_semaine(df_velo, filtre = TRUE)
+  resultat_sans <- calcul_distribution_semaine(df_velo, filtre = FALSE)
+
+  expect_s3_class(resultat_sans, "data.frame")
+  expect_gte(sum(resultat_sans$trajets), sum(resultat_filtre$trajets))
+})
+
+test_that("calcul_distribution_semaine filtre TRUE par défaut", {
+  resultat_defaut <- calcul_distribution_semaine(df_velo)
+  resultat_true <- calcul_distribution_semaine(df_velo, filtre = TRUE)
+
+  expect_equal(resultat_defaut, resultat_true)
+})
+
+test_that("plot_distribution_semaine avec filtre FALSE", {
+  resultat <- plot_distribution_semaine(df_velo, filtre = FALSE)
+
+  expect_s3_class(resultat, "ggplot")
 })
