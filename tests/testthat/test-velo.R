20892aa2c62d9599dc84f453137a348b31fea865
HamitAhmat <hamitahmat75@gmail.com>
2026-04-13 13:42:42 +0200

    ajout 2 test unit

--- tests/testthat/test-velo.R
+++ tests/testthat/test-velo.R
@@ -1,3 +1,62 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("filtre_anomalie supprime les anomalies", {
+  resultat <- filtre_anomalie(df_velo)
+
+  expect_true(all(is.na(resultat[["Probabilité de présence d'anomalies"]])))
+  expect_true(all(resultat$Total > 0))
+  expect_true(all(resultat$Total < 10000))
+  expect_lt(nrow(resultat), nrow(df_velo))
+})
+
+test_that("compter_nombre_trajets retourne un nombre positif", {
+  resultat <- compter_nombre_trajets(df_velo)
+
+  expect_type(resultat, "double")
+  expect_gt(resultat, 0)
+})
+
+test_that("compter_nombre_boucle retourne le bon nombre", {
+  resultat <- compter_nombre_boucle(df_velo)
+
+  expect_type(resultat, "integer")
+  expect_gt(resultat, 1)
+})
+
+test_that("trouver_trajet_max retourne les bonnes colonnes", {
+  resultat <- trouver_trajet_max(df_velo)
+
+  expect_s3_class(resultat, "data.frame")
+  expect_true("Boucle de comptage" %in% colnames(resultat))
+  expect_true("Jour" %in% colnames(resultat))
+  expect_true("Total" %in% colnames(resultat))
+  expect_true("moyenne_jour_identique" %in% colnames(resultat))
+  expect_true("moyenne_boucle_identique" %in% colnames(resultat))
+  expect_equal(nrow(resultat), 1)
+})
+
+test_that("calcul_distribution_semaine retourne 7 jours", {
+  resultat <- calcul_distribution_semaine(df_velo)
+
+  expect_s3_class(resultat, "data.frame")
+  expect_true("trajets" %in% colnames(resultat))
+  expect_lte(nrow(resultat), 7)
+
+})
+
+test_that("plot_distribution_semaine retourne un ggplot", {
+  resultat <- plot_distribution_semaine(df_velo)
+
+  expect_s3_class(resultat, "ggplot")
+})
+
+test_that("filtrer_trajet filtre les bonnes boucles", {
+  resultat <- filtrer_trajet(df_velo, boucle = c(880, 881))
+
+  expect_lt(nrow(resultat), nrow(df_velo))
+  expect_true(all(resultat[["Numéro de boucle"]] %in% c(880, 881)))
+})
+
+test_that("filtrer_trajet avec une seule boucle", {
+  resultat <- filtrer_trajet(df_velo, boucle = 949)
+
+  expect_true(all(resultat[["Numéro de boucle"]] == 949))
 })
